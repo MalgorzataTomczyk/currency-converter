f71c90556e7ab152c94a6add8ae442165dfc3569
Małgorzata Tomczyk <mtomczyk2014@gmail.com>
2024-08-31 17:15:29 +0200

    Function added

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+function convertCurrency(amount, currency) {
+    const EUR = 4.2769;
+    const USD = 3.8453;
+    const GBP = 5.0486;
+    
+    switch (currency) {
+        case "EUR":
+            return amount / EUR;
+        case "USD":
+            return amount / USD;
+        case "GBP":
+            return amount / GBP;
+        default:
+            return 0;
+    }
+}
+
+function updateResult(resultElement, amount, result, currency) {
+    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+}
+
+const formElement = document.querySelector(".js-form");
+const amountElement = document.querySelector(".js-amount");
+const currencyElement = document.querySelector(".js-currency");
+const resultElement = document.querySelector(".js-result");
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
 
-    let result;
+    const result = convertCurrency(amount, currency);
 
-    let EUR = 4.2769;
-    let USD = 3.8453;
-    let GBP = 5.0486;
-
-    switch (currency) {
-        case "EUR":
-            result = amount / EUR;
-            break;
-
-        case "USD":
-            result = amount / USD;
-            break;
-
-        case "GBP":
-            result = amount / GBP;
-            break;
-    }
-    resultElement.innerHTML = `${amount.toFixed(2)} PLN = </strong>${result.toFixed(2)} ${currency}</strong>`;
-
+    updateResult(resultElement, amount, result, currency);
 });
